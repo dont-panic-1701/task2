0f6e15ba29cd882eaad62b3518cf2d8dae234912
dont-panic-1701 <G00dluckcupc@k3>
2020-03-28 18:09:25 +0300

    no tests yet

--- src/main/java/du/Du.java
+++ src/main/java/du/Du.java
@@ -1,7 +1,42 @@
 package du;
 
 
+import java.io.File;
+import java.util.List;
+@SuppressWarnings("WeakerAccess")
 
 public class Du {
+
+    public Du() { } //what is this
+
+    public long getSize(File path) { // possible: directory is currently modified??
+        long size = 0;
+
+        if (path.isDirectory()) {
+            File[] files = path.listFiles();
+            if (files != null){ //nothing to do if path is an empty folder
+                for (File file:files) {
+                    size += getSize(file);
+                }
+            }
+        }
+        else {
+            size += path.length();
+        }
+
+        return size;
+    }
+
+    public long[] getSizes (List<String> paths){
+
+        long[] sizes = new long[paths.size()];
+
+        for (int i = 0; i < paths.size(); i++) {
+            File file = new File(paths.get(i));
+            sizes[i] = getSize(file);
+        }
+
+        return sizes;
+    }
 
 }
--- src/main/java/du/DuLauncher.java
+++ src/main/java/du/DuLauncher.java
@@ -5,10 +5,13 @@ import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.Option;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
 public class DuLauncher {
+
+
 
     @Option(name = "-h", usage = "Include format")
     private boolean hFormat = true;
@@ -17,14 +20,28 @@ public class DuLauncher {
     private boolean cSum = true;
 
     @Option(name = "--si", usage = "Use base 1000")
-    private boolean siBase = true;
+    private boolean changeBase = true;
 
-    @Argument(required = true, usage = "Files names")
-    private String fileNames;
+    @Argument(required = true, usage = "File paths")
+    private List<String> paths = new ArrayList<String>();
 
     public static void main(String[] args) {
         new DuLauncher().launch(args);
     }
+
+
+    private static String getUnit(long size, int base) {
+        List<String> units = new ArrayList<String>(Arrays.asList("B", "KB", "MB", "GB"));
+        int unit = 0;
+        for(int i = 1; i < 4; i++){
+            if (size/base == 0)
+                break;
+            size /= base;
+            unit = i;
+        }
+        return size + " " + units.get(unit);
+    }
+
 
     private void launch(String[] args) {
         CmdLineParser parser = new CmdLineParser(this);
@@ -37,6 +54,21 @@ public class DuLauncher {
             parser.printUsage(System.err);
             return;
         }
+
+        Du du = new Du();
+        int base = changeBase ? 1000 : 1024; //if there was no --si flag will changeBase be null?
+
+        long[] sizes = du.getSizes(paths);
+        System.out.println("Requested files sizes:");
+        long sum = 0;
+        for(long size: sizes){
+            if (cSum) {
+                sum += size;
+            } else {
+                System.out.println(hFormat ? getUnit(size, base) : size);
+            }
+        }
+        if (cSum) System.out.println(hFormat? getUnit(sum, base): sum);
 
     }
 }
